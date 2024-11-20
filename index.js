d2a3830fd9e4031ceee60bed10a7820b95eb1979
Nawaf almehdhar <181556586+almehdharHub@users.noreply.github.com>
2024-11-20 21:07:32 +0300

    adding other operations

--- index.js
+++ index.js
@@ -45,13 +45,26 @@ app.get("/", (req, res) => {
   res.send("<h1>Hello World!</h1>");
 });
 
-const generateId = () => {
-  const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
-  return maxId + 1;
-};
+app.get("/api/notes", (request, response) => {
+  Note.find({}).then((notes) => {
+    response.json(notes);
+  });
+});
+app.get("/api/notes/:id", (request, response, next) => {
+  Note.findById(request.params.id)
+    .then((note) => {
+      if (note) {
+        response.json(note);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
+});
 
 app.post("/api/notes", (request, response) => {
   const body = request.body;
+  console.log("content body", body.content);
 
   if (body.content === undefined) {
     return response.status(400).json({ error: "content missing" });
@@ -66,25 +79,44 @@ app.post("/api/notes", (request, response) => {
     response.json(savedNote);
   });
 });
+app.put("/api/notes/:id", (request, response, next) => {
+  const body = request.body;
 
-app.get("/api/notes", (request, response) => {
-  Note.find({}).then((notes) => {
-    response.json(notes);
-  });
+  const note = {
+    content: body.content,
+    important: body.important,
+  };
+
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then((updatedNote) => {
+      response.json(updatedNote);
+    })
+    .catch((error) => next(error));
 });
-
 app.delete("/api/notes/:id", (request, response) => {
-  const deletedNote = request.params.id;
-  Note.deleteOne({ _id: deletedNote }).then((note) => {
-    response.json(note);
-  });
+  Note.findByIdAndDelete(request.params.id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
+  // const deletedNote = request.params.id;
+  // Note.deleteOne({ _id: deletedNote }).then((note) => {
+  //   response.json(note);
+  // });
 });
 
-app.get("/api/notes/:id", (request, response) => {
-  Note.findById(request.params.id).then((note) => {
-    response.json(note);
-  });
-});
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  }
+
+  next(error);
+};
+
+// this has to be the last loaded middleware, also all the routes should be registered before this!
+app.use(errorHandler);
 
 app.use(unknownEndpoint);
 
